5b052bae4781f68493d270d21868acf4739b411f
Evgeny Burukhin <evgenylaptop0@gmail.com>
2026-02-28 15:28:52 +1000

    Task 2

--- lab1/test1.py
+++ lab1/test1.py
@@ -1,2 +1,7 @@
-print(121)
-print(3)
+# B2
+def intersection(arr1, arr2):
+    return list(set(arr1) & set(arr2))
+
+print(intersection([1, 2, 2, 3], [2, 3, 4]))
+print(intersection([1, 2], [3, 4]))
+print(intersection([], [1, 2]))
